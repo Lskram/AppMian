1dcea93536025532a45377aa37245ca6024cb7f1
Lskram <dankflamemasterLove@gmail.com>
2025-06-12 23:43:31 +0700

    ปุ่ม TextButton,FilledButton,OutlinedButton,ElevatedButton

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -10,7 +10,7 @@ void main() {
           backgroundColor: Colors.amber,
           centerTitle: true,
         ),
-        body: const Homee(),
+        body: const Home(),
       )
     )
   );
--- myapp/lib/screens/home.dart
+++ myapp/lib/screens/home.dart
@@ -5,41 +5,47 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
-        Image.network(
-          "https://staticg.sportskeeda.com/editor/2024/05/5dbc5-17159017650761-1920.jpg?w=640",
-          width: 150,
-          height: 150,
-        ),
-        SizedBox(height: 10),
-        Image.asset(
-          "assets/images/pic1.png", 
-          width: 150, 
-          height: 150
-        ),
-      ],
-    );
-  }
-}
-
-class Homee extends StatelessWidget {
-  const Homee({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
-        Image.network(
-          "https://static.wikia.nocookie.net/jujutsu-kaisen/images/5/5a/Satoru_Gojo_arrives_on_the_battlefield_%28Anime%29.png/revision/latest?cb=20210226205256",
-          width: 150,
-          height: 150,
-        ),
-      ],
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          TextButton(
+            onPressed: () {
+              print("ปุ่มกด");
+            },
+            child: const Text(
+              "TextButton",
+              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+            ),
+          ),
+          SizedBox(height: 20),
+          FilledButton(
+            onPressed: () {
+              print("กดหน่อย");
+            },
+            child: Text(
+              "FilledButton",
+              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+            ),
+          ),
+          SizedBox(height: 25),
+          OutlinedButton(
+            onPressed: () {},
+            child: const Text(
+              "OutlineButton",
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            ),
+          ),
+          SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () {},
+            child: Text(
+              "ElevatedButton",
+              style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
