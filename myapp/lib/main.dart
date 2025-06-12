d10c6eb3a0e72115910d0c7616f0bc161da1b2f1
Lskram <dankflamemasterLove@gmail.com>
2025-06-12 23:29:02 +0700

    Image.network และ Image.asset (pubspec.yaml

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -10,7 +10,7 @@ void main() {
           backgroundColor: Colors.amber,
           centerTitle: true,
         ),
-        body: const Home(),
+        body: const Homee(),
       )
     )
   );
--- myapp/lib/screens/home.dart
+++ myapp/lib/screens/home.dart
@@ -5,24 +5,39 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-        Container(
-          color: Colors.deepOrange,
-          padding: const EdgeInsets.all(5),
-          child: Text("TLE", style: TextStyle(fontSize: 30, letterSpacing: 3)),
+        Image.network(
+          "https://staticg.sportskeeda.com/editor/2024/05/5dbc5-17159017650761-1920.jpg?w=640",
+          width: 150,
+          height: 150,
         ),
-        Container(
-          color: Colors.deepOrange,
-          padding: const EdgeInsets.all(5),
-          child: Text("TLE", style: TextStyle(fontSize: 30, letterSpacing: 3)),
+        SizedBox(height: 10),
+        Image.asset(
+          "assets/images/pic1.png", 
+          width: 150, 
+          height: 150
         ),
-        Container(
-          color: Colors.deepOrange,
-          padding: const EdgeInsets.all(5),
-          child: Text("TLE", style: TextStyle(fontSize: 30, letterSpacing: 3)),
+      ],
+    );
+  }
+}
+
+class Homee extends StatelessWidget {
+  const Homee({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Image.network(
+          "https://static.wikia.nocookie.net/jujutsu-kaisen/images/5/5a/Satoru_Gojo_arrives_on_the_battlefield_%28Anime%29.png/revision/latest?cb=20210226205256",
+          width: 150,
+          height: 150,
         ),
       ],
     );
