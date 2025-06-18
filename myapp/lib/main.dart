85696732fe82d89626088124f712f9dcd882c745
Lskram <dankflamemasterLove@gmail.com>
2025-06-18 07:04:19 +0700

    st Fulwidget and add,subtract Number and Void

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:myapp/screens/home.dart';
+import 'package:myapp/screens/item.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,11 +15,11 @@ class MyApp extends StatelessWidget {
       title: "My app",
       home: Scaffold(
         appBar: AppBar(
-          title: const Text("My App"),
+          title: const Text("หน้าแรกของแอปพลิเคชั่น"),
           backgroundColor: Colors.amber,
           centerTitle: true,
         ),
-        body: const Home(),
+        body: const Item(),
       ),
     );
   }
---
+++ myapp/lib/screens/item.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class Item extends StatefulWidget {
+  const Item({super.key});
+
+  @override
+  State<Item> createState() => _ItemState();
+}
+
+class _ItemState extends State<Item> {
+  int quantity = 5;
+  void addQuantity() {
+    setState(() {
+      quantity += 1;
+    });
+  }
+
+  void subtractQuantity() {
+    setState(() {
+      quantity <= 0 ? 0 : quantity -= 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            "$quantity",
+            style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 20),
+          OutlinedButton(
+            onPressed: addQuantity,
+            child: Text(
+              "+",
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            ),
+          ),
+          SizedBox(height: 20),
+          OutlinedButton(
+            onPressed: subtractQuantity,
+            child: Text(
+              "-",
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
