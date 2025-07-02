cd21229791c42ceb713978857866bf09411b662b
Lskram <dankflamemasterLove@gmail.com>
2025-07-02 18:39:59 +0700

    เริ่มสร้าง Home

--- myapp/lib/screens/home.dart
+++ myapp/lib/screens/home.dart
@@ -1,75 +1,29 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
 
-class Home extends StatefulWidget {
+class Home extends StatelessWidget {
   const Home({super.key});
-
-  @override
-  State<Home> createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
-  late Timer _timer;
-  late DateTime _now;
-  List<String> notifications = [
-    "08:00 น. - ตื่นนอน",
-    "09:00 น. - ประชุมทีม",
-    "12:00 น. - ทานข้าวกลางวัน"
-  ];
-
-  @override
-  void initState() {
-    super.initState();
-    _now = DateTime.now();
-    _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
-      setState(() {
-        _now = DateTime.now();
-      });
-    });
-  }
-
-  @override
-  void dispose() {
-    _timer.cancel();
-    super.dispose();
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("แจ้งเตือนของฉัน")),
-      body: Column(
-        children: [
-          const SizedBox(height: 30),
-          // นาฬิกา
-          Center(
-            child: Text(
-              "${_now.hour.toString().padLeft(2, '0')}:${_now.minute.toString().padLeft(2, '0')}:${_now.second.toString().padLeft(2, '0')}",
-              style: const TextStyle(fontSize: 48, fontWeight: FontWeight.bold),
-            ),
-          ),
-          const SizedBox(height: 30),
-          const Text(
-            "รายการแจ้งเตือน",
-            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-          ),
-          const SizedBox(height: 10),
-          Expanded(
-            child: ListView.builder(
-              itemCount: notifications.length,
-              itemBuilder: (context, index) => ListTile(
-                leading: const Icon(Icons.notifications),
-                title: Text(notifications[index]),
-              ),
-            ),
-          ),
-        ],
+      //ส่วนหัวของแอป
+      // AppBar เป็น widget ที่ใช้แสดงส่วนหัวของแอป
+      appBar: AppBar(
+        title: Text(
+          "Home",
+          style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
+        ),
+        backgroundColor: Colors.green.shade400,
+        centerTitle: true,
+        elevation: 0,
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          // เพิ่มฟังก์ชันเพิ่มแจ้งเตือนที่นี่
-        },
-        child: const Icon(Icons.add),
+      //ส่วนของ body ของแอป
+      // ในที่นี้เราจะใช้ Container ว่างๆ
+      body: Center(
+        child: Text(
+          "Hello Flutter!",
+          style: TextStyle(fontSize: 24),
+        ),
       ),
     );
   }
